258aa416f9eace5c77159ebabebcb5c4e49f1c20
Ramon Fernandes <ramonfernandes9@gmail.com>
2021-02-02 23:45:21 -0300

    Aula 01 javascript

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+var nome = "Ramon Fernandes";
+var idade = 38;
+var idade2 = 23;
+var frase = "Vasco é o melhor time do mundo!";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+
+console.log(nome);
+console.log(idade + idade2);
+//console.log(frase.replace("melhor", "pior"));
+console.log(frase.toUpperCase());
+alert(frase.replace("melhor", "pior"));
+
+
+
