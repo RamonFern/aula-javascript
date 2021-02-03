bfe08647b01778aca6486185410fabb160524f7e
Ramon Fernandes <ramonfernandes9@gmail.com>
2021-02-03 15:54:54 -0300

    finalizando curso javascript iniciante

--- js/main.js
+++ js/main.js
@@ -1,11 +1,71 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado de novo";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar");
+}
 
+function redirecionar(){
+    window.open("https://github.com/RamonFern");
+    //window.location.href = "https://github.com/RamonFern";
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    elemento.innerHTML = "Obrigado por passar o mouse";
+    //alert("Trocar texto");
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = ""Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+function load(){
+    alert("Página carregada");
+}
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+
+
+
+
+
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+
+var validar;
+function validarIdade(idade){
+    
+    if(idade >= 18){
+        validar = true;
+    }else{
+        validar = false;
+    }
+    return validar
+}
+
+var idade = prompt("Qual sua idade?");
+validarIdade(idade);
+console.log(validar);
+alert(soma(2,3));
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+}
+*/
+
+//alert(setReplace("Vai Argentina", "Argentina", "Brasil"));
+
+/*
 var d = new Date();
 alert(d.getDay());
 alert(d.getHours());
 alert(d.getMinutes());
 
-
-/*
 var cont;
 for(cont =0; cont <= 5; cont++){
     alert(cont);
