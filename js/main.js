f7d795eaed4bfa995a085977ad68f76cdcc8139b
Ramon Fernandes <ramonfernandes9@gmail.com>
2021-02-03 00:15:16 -0300

    aula 02 javascript

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
+var frutas = [{nome:"jaca", cor:"amarela encardida"}, {nome:"manga", cor:"vermelha"}]
+console.log(frutas);
+alert(frutas[1].nome);
 
+
+
+/*
+var fruta = {nome:"jaca", cor:"amarela encardida"}
+console.log(fruta.name);
+alert(fruta.cor);
+
+var lista = ["maçã", "pêra", "laranja"];
+lista.push("manga");
+lista.pop();
+
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" - "));
+*/
+/*
 var nome = "Ramon Fernandes";
 var idade = 38;
 var idade2 = 23;
 var frase = "Vasco é o melhor time do mundo!";
-//alert(nome + " tem " + idade + " anos");
-//alert(idade + idade2);
+alert(nome + " tem " + idade + " anos");
+alert(idade + idade2);
 
 console.log(nome);
 console.log(idade + idade2);
-//console.log(frase.replace("melhor", "pior"));
+console.log(frase.replace("melhor", "pior"));
 console.log(frase.toUpperCase());
 alert(frase.replace("melhor", "pior"));
+*/
 
 
 
