40e526e473494120fe07a9fbb32390de59d291a2
Ramon Fernandes <ramonfernandes9@gmail.com>
2021-02-03 00:42:07 -0300

    aula 03 javascript

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
+
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
+
+/*
+var cont;
+for(cont =0; cont <= 5; cont++){
+    alert(cont);
+}
+
+
+var count = 0;
+while(count <= 5){
+    console.log(count);
+    //alert(count);
+    count++;
+}
+
+
+var idade = prompt("Qual sua idade?");
+if(idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+};
+
 var frutas = [{nome:"jaca", cor:"amarela encardida"}, {nome:"manga", cor:"vermelha"}]
 console.log(frutas);
 alert(frutas[1].nome);
 
-
-
-/*
 var fruta = {nome:"jaca", cor:"amarela encardida"}
 console.log(fruta.name);
 alert(fruta.cor);
